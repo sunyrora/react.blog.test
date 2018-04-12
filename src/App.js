6df5727ce44feda0de49e696b758c44f97777adc
sunyrora <sunyrora@gmail.com>
2018-04-12 15:55:34 +0200

    BrowserRouter -> Router, user hashHistory for hard refresh on github pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router';
+import createHistory from "history/createHashHistory"
 import Header from './components/Header';
 import Main from './components/Main';
 // import Game from './components/Game';
 
+const history = createHistory();
+
 class App extends Component {
 
   render() {
+    console.log("process.env.PUBLIC_URL: ", process.env.PUBLIC_URL);
     return (
-      <BrowserRouter>
+      <Router basename={process.env.PUBLIC_URL} history={history}>
         <div>
           <Header />
           <Main />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
