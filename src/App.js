5de7b92cde05aa3df180fbda51690e7efd631c67
sunyrora <sunyrora@gmail.com>
2018-04-12 16:58:34 +0200

    Remove: history package, use HashRouter from 'react-router-dom' instead

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 // import { BrowserRouter } from 'react-router-dom';
-import { Router } from 'react-router';
+import { HashRouter as Router } from 'react-router-dom';
+// import { Router } from 'react-router';
 import createHistory from "history/createHashHistory"
 import Header from './components/Header';
 import Main from './components/Main';
 // import Game from './components/Game';
 
-const history = createHistory();
+// const history = createHistory();
 
 class App extends Component {
 
   render() {
     console.log("process.env.PUBLIC_URL: ", process.env.PUBLIC_URL);
     return (
-      <Router basename={process.env.PUBLIC_URL} history={history}>
+      <Router basename={process.env.PUBLIC_URL}>
         <div>
           <Header />
           <Main />
